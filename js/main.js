edafbe26a5be46112d077d87ff1befe4bfdb48ff
SteNeithan <steneithan000@gmail.com>
2026-08-27 11:09:44 -0600

    acomode en su lugar el loading

--- js/main.js
+++ js/main.js
@@ -55,7 +55,7 @@ function pintarJuegos(listaDeJuegos) {
     const idUnico = juego.id || juego.id_juego || 1;
     const tarjetaHTML = `
       <div class="tarjeta-juego">
-        <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}">
+        <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}" loading="lazy">
         <div class="info-juego">
           <!-- Aquí mandamos llamar la plataforma usando el nombre de tu BD -->
           <span class="consola">${juego.juegos_plataformas || juego.consola || juego.plataforma || 'Consola'}</span>
@@ -68,7 +68,6 @@ function pintarJuegos(listaDeJuegos) {
     `;
     contenedor.innerHTML += tarjetaHTML;
   });
-  <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}" loading="lazy"></img>
 }
 
 function pintarJuegosCategorias(lista) {
@@ -93,7 +92,7 @@ function pintarJuegosCategorias(lista) {
     if (!juegosFusionados[tituloJuego]) {
       // Hacemos una copia del juego y le creamos una lista vacía para sus categorías
       juegosFusionados[tituloJuego] = { ...juego, listaCategorias: [] };
-      <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}" loading="lazy"></img>
+      
     }
 
     // Rescatamos la categoría actual asegurándonos de que exista y NO sea "Todos"
@@ -120,7 +119,7 @@ function pintarJuegosCategorias(lista) {
 
     const tarjetaHTML = `
       <div class="tarjeta-juego">
-        <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}">
+        <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}" loading="lazy">
         <div class="info-juego">
           <span class="consola">${juego.juegos_plataformas || juego.consola || juego.plataforma || 'Consola'}</span>
           <h3>${juego.titulo}</h3>
@@ -145,7 +144,7 @@ function pintarJuegosAdmin(listaDeJuegos) {
     const idUnico = juego.id || juego.id_juego || 1;
     const tarjetaHTML = `
       <div class="tarjeta-juego">
-        <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}">
+        <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}" loading="lazy">
         <div class="info-juego">
           <h3>${juego.titulo}</h3>
           <p class="consola">ID: ${idUnico}</p>
@@ -156,7 +155,7 @@ function pintarJuegosAdmin(listaDeJuegos) {
     `;
     contenedor.innerHTML += tarjetaHTML;
   });
-  <img src=".${juego.imagen_url || ''}" alt="${juego.titulo}" loading="lazy"></img>
+  
 }
 
 /* =========================================================================
